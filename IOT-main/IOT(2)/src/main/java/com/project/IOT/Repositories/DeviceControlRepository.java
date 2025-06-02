978f868707af6bd28bc56ee750aa228d215a704e
bangtrinh <tn2003112@gmail.com>
2025-06-02 17:58:12 +0700

    [edit] control

--- IOT-main/IOT(2)/src/main/java/com/project/IOT/Repositories/DeviceControlRepository.java
+++ IOT-main/IOT(2)/src/main/java/com/project/IOT/Repositories/DeviceControlRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface DeviceControlRepository extends JpaRepository<DeviceControl, Long> {
     List<DeviceControl> findByUserId(Long userId);
     Optional<DeviceControl> findByUserIdAndDeviceId(Long userId, Long deviceId);
+    boolean existsByUserIdAndDeviceId(Long userId, Long deviceId);
 }
